853268829cfe233f3032da27047e0a7b8e171967
Chris O'Neil <chris.oneil@gmail.com>
2021-07-11 15:28:08 +0100

    Allow Specification of Subtasks
    This uses a very simple method that only allows for 1 level of subtask,
    rather than allowing an arbitrary level using recursion or something
    like that. All I need is just a single level.
    
    A task is denoted by a leading '*' and a subtask is denoted with a
    leading '-'. The text file can be made more readable using indentation,
    but any leading whitespace will be stripped during processing.
    
    Also corrects a bad reference to a function that had changed names.

--- add-tasks-to-new-section.py
+++ add-tasks-to-new-section.py
@@ -6,7 +6,7 @@ import sys
 
 from todoist.api import TodoistAPI
 from labels import get_label_ids
-from projects import get_user_project_selection
+from projects import ui_select_work_project
 
 api_token = os.getenv('TODOIST_API_TOKEN')
 api = TodoistAPI(api_token)
@@ -19,20 +19,35 @@ def create_section(project_id):
 
 def parse_line(line):
     split = line.strip().split(';')
+    task_name = split[0].strip()
+    is_sub_task = True if task_name[0] == '-' else False
     labels = ast.literal_eval(split[1].strip())
-    return (split[0], [l.strip() for l in labels])
+    task_name = task_name[1:].strip() # remove leading '* ' or '- ' from task name
+    return (task_name, [l.strip() for l in labels], is_sub_task)
 
 def add_tasks_from_file(path, project, section):
     print("Adding {section} section to {project} project".format(
         section=section['name'], project=project[0]))
     with open(path) as f:
+        parent_id = None
         for line in f.readlines():
-            task_name, labels = parse_line(line)
+            task_name, labels, is_sub_task = parse_line(line)
             label_ids = get_label_ids(api, labels)
-            print("Adding {task_name} task to {section} section".format(
-                task_name=task_name, section=section['name']))
-            api.items.add(
-                task_name, project_id=project[1], section_id=section['id'], labels=label_ids)
+            if not is_sub_task:
+                print("Adding {task_name} task to {section} section".format(
+                    task_name=task_name, section=section['name']))
+                item = api.items.add(
+                    task_name, project_id=project[1], section_id=section['id'], labels=label_ids)
+                parent_id = item['id']
+            else:
+                print("Adding {task_name} subtask to {section} section".format(
+                    task_name=task_name, section=section['name']))
+                api.items.add(
+                    task_name,
+                    project_id=project[1],
+                    section_id=section['id'],
+                    labels=label_ids,
+                    parent_id=parent_id)
 
 def get_input_file_from_args():
     if len(sys.argv) < 2:
@@ -49,7 +64,7 @@ def get_input_file_from_args():
 
 def main():
     input_task_file_path = get_input_file_from_args()
-    project = get_user_project_selection(api)
+    project = ui_select_work_project(api)
     section = create_section(project[1])
     add_tasks_from_file(input_task_file_path, project, section)
     api.commit()
